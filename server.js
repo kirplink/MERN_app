8caef5fe857063b4dbd3cb3ddc435ccbd864f779
daniel 'kirplink' reynolds <=>
2019-03-16 09:32:14 -0500

    testing

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const path = require("path");
 
-//connect to mongdb
-mongoose
-	.connect(process.env.MONGODB_URI || "mongodb://localhost/travelappDB")
-	.then(() => console.log("MongoDB Connected"))
-	.catch(err => console.log(err));
-
-require("./config/passport")(passport);
-
 // Define middleware here
 app.use(morgan("dev"));
 app.use(cookieParser());
@@ -29,19 +21,30 @@ app.use(session({ secret: "letsgoflyakite" }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// routes
-require("./routes/api/userAuth")(app, passport);
-app.use(routes);
-
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 	//set static folder
-	app.use(express.static("client/build"));
+	// ... other app.use middleware
+	app.use(express.static(path.join(__dirname, "client", "build")));
 
-	// app.get("*", (req, res) => {
-	// 	res.sendFile(path.resolve(__dir, "client", "build", "index.html"));
-	// });
+	// ...
+	// Right before your app.listen(), add this:
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
 }
+
+//connect to mongdb
+mongoose
+	.connect(process.env.MONGODB_URI || "mongodb://localhost/travelappDB")
+	.then(() => console.log("MongoDB Connected"))
+	.catch(err => console.log(err));
+
+require("./config/passport")(passport);
+
+// routes
+require("./routes/api/userAuth")(app, passport);
+app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
